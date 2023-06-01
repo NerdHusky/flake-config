95b2353a6196b90161121c4c5ac5e1d287a5aa40
Carlo Cuoghi <nerdhusky86@gmail.com>
2023-06-01 15:38:37 +0200

    cleaning

--- home/carlo/features/desktop/x11.old/rofi/default.nix
+++
@@ -1,8 +0,0 @@
-{
-	programs.rofi.enable = true;
-
-	home.file."/home/carlo/.config/rofi/".recursive = true;
-	home.file."/home/carlo/.config/rofi/".source = ./rofi;
-
-	programs.rofi.configPath = "/home/carlo/.config/config.rasi";
-}
